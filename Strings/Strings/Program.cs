a7b3443a925c83c183d6347062ae8334869593e6
Loredana Radu <loredana.radu@scoalainformala.ro>
2024-04-11 19:21:54 +0300

    concatenation examples

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -4,6 +4,10 @@
     {
         // Declare strings
         string myFirstString = "Hello world!";
+        //same declarations
+        //System.String myFirstString;
+        //String myFirstString;
+
         Console.WriteLine(myFirstString);
 
         int myNumber = 42;
@@ -11,6 +15,16 @@
 
         string myNumberAsString = myNumber.ToString();
 
+        // Manipulating strings;
+        // Concatenation
+        string firstName = "Ionel";
+        string lastName = "Popescu";
+        string fullName = firstName + " " + lastName;
 
+
+        string fullNameWithConcat = string.Concat(firstName, " ", lastName);
+        Console.WriteLine(fullName);
+        Console.WriteLine(fullNameWithConcat);
+        Console.WriteLine($"{firstName} {lastName}"); //string interpolation
     }
 }
