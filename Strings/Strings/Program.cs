2a6f21acc101ee7db88df0eac226b67ec6293372
Loredana Radu <loredana.radu@scoalainformala.ro>
2024-04-11 19:00:29 +0300

    initial commit for strings course

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿public static class Program 
+{
+    public static void Main()
+    {
+        // Declare strings
+        string myFirstString = "Hello world!";
+        Console.WriteLine(myFirstString);
+
+        int myNumber = 42;
+        Console.WriteLine(myNumber);
+
+        string myNumberAsString = myNumber.ToString();
+
+
+    }
+}
