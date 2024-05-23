f1fa97326f33a1a6a10bea72c437802c42d4f8a7
Loredana Radu <loredana.radu@scoalainformala.ro>
2024-05-23 21:07:42 +0300

    queues and dictionaries

---
+++ Collections2/Dictionaries/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare
+            Dictionary<string, double> studentGrades = new Dictionary<string, double>();
+
+            // add items
+            studentGrades.Add("Maria", 7.5);
+            studentGrades.Add("Barbu", 8);
+            studentGrades.Add("Ion", 5.6);
+            studentGrades.Add("Greta", 9.2);
+            //studentGrades.Add("Greta", 10); // keys in dict are unique
+
+            // print
+            foreach (var student in studentGrades)
+            {
+                Console.WriteLine($"{student.Key} --> {student.Value}");
+            }
+
+            Console.WriteLine();
+
+            // print only keys
+            var keys = studentGrades.Keys;
+            foreach (var student in keys)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine();
+            // print only values
+            var values = studentGrades.Values;
+            foreach (var grade in values)
+            {
+                Console.WriteLine(grade);
+            }
+
+            //v1 change the value in a dictionary
+            // check if value exists
+            if (studentGrades.ContainsKey("Barbu"))
+            {
+                // change the grade
+                studentGrades["Barbu"] = 10; // Prints: Barbu, 10
+            }
+
+            //v2
+            double tempValue = 0;
+            bool barbuExists = studentGrades.TryGetValue("Barbu", out tempValue);
+            Console.WriteLine("Is Barbu in the dictionary?" + barbuExists);
+
+            // Complex dictionary
+            //inline initialization
+            Dictionary<string, List<int>> complexDict = new Dictionary<string, List<int>>
+            {
+                { "Maria", new List<int>{ 8, 9, 7 } },
+                { "Harry", new List<int>() }
+            };
+
+
+            // dict of students and course with grades 
+            Dictionary<string, Dictionary<string, List<double>>> studentsWithGradesPerCourse = 
+                new Dictionary<string, Dictionary<string, List<double>>>
+            {
+                { "Maria",
+                    new Dictionary<string, List<double>> {
+                        { "Math", new List<double> { 8, 9, 7 } },
+                        { "Music", new List<double> { 6, 9.7, 7.5 } },
+                    }
+                },
+                { "Victor",
+                    new Dictionary<string, List<double>> {
+                        { "Informatics", new List<double> { 8, 9, 7 } },
+                        { "Phisics", new List<double> { 6, 9.7, 7.5 } },
+                    }
+                },
+            };
+        }
+    }
+}
---
+++ Collections2/Queues/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare a queue
+            Queue<string> lunchQueue = new Queue<string>();
+
+            lunchQueue.Enqueue("Maria");
+            lunchQueue.Enqueue("Gigi");
+            lunchQueue.Enqueue("John");
+            lunchQueue.Enqueue("Eva");
+
+            // check the first element of the queue
+            Console.WriteLine($"First person in line is {lunchQueue.Peek()}");
+
+            Console.WriteLine($"Is Gigi still in queue? {lunchQueue.Contains("Gigi")}");
+
+            // removes all the elements in the queue
+            while (lunchQueue.Count > 0)
+            {
+                var nextInLine = lunchQueue.Dequeue();
+                Console.WriteLine(nextInLine);
+            }
+
+            //or
+
+            lunchQueue.Clear();
+
+            //Queue<int> intQueue = new Queue<int> { 1, 2, 3 }; // not possible
+
+        }
+    }
+}
