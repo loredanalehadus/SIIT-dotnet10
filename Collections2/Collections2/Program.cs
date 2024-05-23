1318bcbe716754c30b50fc982ef50b0823afb1bc
Loredana Radu <loredana.radu@scoalainformala.ro>
2024-05-23 19:52:03 +0300

    Lists and Stacks

---
+++ Collections2/Collections2/Program.cs
@@ -0,0 +1,80 @@
+﻿using Lists;
+using System.Collections;
+
+namespace Collections2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare a list
+            List<string> list = new List<string>();
+            Console.WriteLine($"Number of elements in the list: {list.Count}");
+
+            //add elements
+            list.Add("lapte");
+            list.Add("oua");
+            list.Add("salata");
+            list.Add("ciocolata");
+
+            //ArrayList oldList = new ArrayList();
+            //oldList.Add("lapte");
+            //oldList.Add(12);
+            //oldList.Add(new object());
+
+            Console.WriteLine($"Number of elements in the list: {list.Count}");
+
+            // Console.WriteLine(list);
+            // union of list
+            List<string> wifeShoppingList = new List<string> { "ciocolata", "cafea", "pantofi", "parfum" };
+
+            list.AddRange(wifeShoppingList);
+
+            // print elements of a list
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            // sort the list
+            list.Sort();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            // remove an element
+            //list.Remove("ciocolata");
+            list.Remove("pantofi");
+
+            list.RemoveAll(x => x == "ciocolata");
+
+            Console.WriteLine($"Mai exista pantofi in list? {list.Contains("ciocolata")}");
+            Console.WriteLine($"Mai exista pantofi in list? {list.Contains("pantofi")}");
+
+            // reverse 
+            List<int> intList = new List<int> { 5, 4, 3, 2, 1 };
+            intList.Reverse();
+
+            Person person1 = new Person("Vasile", "Popescu");
+            Person person2 = new Person("Emilia", "Nae");
+
+            List<Person> people = new List<Person>();
+            people.Add(person1);
+            people.Add(person2);
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.FirstName} {person.LastName}");
+            }
+
+            Console.WriteLine(people[1].FirstName);
+
+            //clear elements
+            people.Clear();
+            Console.WriteLine($"Number of elements in the list: {people.Count}");
+
+        }
+    }
+}
---
+++ Collections2/Stack/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare stack
+            Stack<string> cities = new Stack<string>();
+
+            //add elements
+            cities.Push("1. Iasi");
+            cities.Push("2. Cluj");
+            cities.Push("3. Brasov");
+            cities.Push("4. Bucuresti");
+
+            //check the last element without removing it
+            Console.WriteLine($"Last element is: {cities.Peek()}");
+
+            //removes the last element
+            cities.Pop();
+            Console.WriteLine($"Last element is: {cities.Peek()}");
+
+            while (cities.Count > 0)
+            {
+                Console.WriteLine(cities.Pop());
+            }
+
+            PrintReversedIntegers();
+        }
+
+        static void PrintReversedIntegers()
+        {
+            //reading from relative path
+            var myStack = ReadFromFile("input.txt");
+
+            //reading from absolute path
+            //var myStack = ReadFromFile("C:/Users/catal/Desktop/sample.txt");
+
+            Console.WriteLine("Reversed integers:");
+            while (myStack.Count > 0)
+            {
+                int number = myStack.Pop();
+                Console.WriteLine(number);
+            }
+        }
+
+        static Stack<int> ReadFromFile(string fileName)
+        {
+            var intStack = new Stack<int>();
+
+            // check if file exists
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine($"File {fileName} is not found");
+                return intStack;
+            }
+
+            var reader = new StreamReader(fileName);
+            using (reader)
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (int.TryParse(line, out int value))
+                    {
+                        intStack.Push(value);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid integer in input: {line}");
+                    }
+                }
+            }
+
+            return intStack;
+        }
+
+    }
+}
