778e09543f7f2c4785945ce8fee71f3f3afdcf0e
Loredana Radu <loredana.radu@scoalainformala.ro>
2024-06-11 17:26:05 +0300

    Added First Web Api project

---
+++ FirstAppi/FirstAppi/Controllers/StudentsController.cs
@@ -0,0 +1,106 @@
+﻿using FirstAppi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAppi.Controllers
+{
+    [Route("api/[controller]")] //localhost:{port}/api/students
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private static List<Student> students = new List<Student>();
+
+        public StudentsController()
+        {
+            students = new List<Student>()
+                {
+                    new Student { Id=1,  FirstName = "John", LastName = "Doe", Age = 20, Grade = 3.8 },
+                    new Student { Id=2, FirstName = "Jane", LastName = "Smith", Age = 21, Grade = 3.6 },
+                    new Student { Id=3, FirstName = "Alice", LastName = "Johnson", Age = 19, Grade = 3.9 },
+                    new Student { Id=4, FirstName = "Bob", LastName = "Williams", Age = 22, Grade = 3.3 },
+                    new Student { Id=5, FirstName = "Emily", LastName = "Brown", Age = 20, Grade = 3.7 }
+                };
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllStudents()//GetAll
+        {
+            return Ok(students);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var student = students.FirstOrDefault(x => x.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+        //CREATE
+        [HttpPost]
+        public IActionResult Create([FromBody] Student student, [FromHeader] string? culture, [FromQuery] string? gender)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            //simulate the db saving
+
+            student.Id = 100;
+            students.Add(student);
+
+
+            return CreatedAtAction(nameof(GetById), new { id = student.Id }, student);
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, Student model)
+        {
+            if (id != model.Id)
+            {
+                return BadRequest();
+            }
+
+            var studentFromDb = students.FirstOrDefault(x => x.Id == id);
+            if (studentFromDb == null)
+            {
+                return NotFound();
+            }
+            //update in the database
+
+            return Ok(studentFromDb);
+            // return Ok();
+        }
+
+
+        [HttpHead("{id}")]
+        public IActionResult CheckIfExists(int id)
+        {
+            if (students.Any(x => x.Id == id))
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var studentWithId = students.FirstOrDefault(x => x.Id == id);
+            if (studentWithId == null)
+            {
+                return NotFound();
+            }
+
+            students.Remove(studentWithId);
+            return NoContent();
+        }
+    }
+}
---
+++ FirstAppi/FirstAppi/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstAppi.Models
+{
+    public class Student
+    {
+        // Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Range(18, 21)]
+        public int Age { get; set; }
+        public int Id { get; set; }
+
+        public double Grade { get; set; }
+    }
+}
