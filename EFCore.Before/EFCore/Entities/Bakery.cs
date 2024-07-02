79977c462e2efc052ca6ef80077c52c372233e02
Loredana Radu <loredana.radu@scoalainformala.ro>
2024-07-02 17:29:01 +0300

    ef core code first

---
+++ EFCore.Before/EFCore/Entities/Bakery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cupcakes.Models
+{
+    public class Bakery
+    {
+        [Key]
+        public int BakeryId { get; set; }
+
+        [StringLength(50, MinimumLength = 4)]
+        public string BakeryName { get; set; }
+
+        [Range(1, 40)]
+        public int Quantity { get; set; }
+
+        [StringLength(50, MinimumLength = 4)]
+        public string Address { get; set; }
+
+        [JsonIgnore]
+        [NotMapped]
+        public virtual ICollection<Cupcake> Cupcakes { get; set; }
+    }
+}
---
+++ EFCore.Before/EFCore/Entities/Cupcake.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Cupcakes.Models
+{
+    public class Cupcake
+    {
+        [Key]
+        public int CupcakeId { get; set; }
+
+        [Required(ErrorMessage = "Please select a cupcake type")]
+        [Display(Name = "Cupcake Type:")]
+        public CupcakeType? CupcakeType { get; set; }
+
+        [Required(ErrorMessage = "Please enter a cupcake description")]
+        [Display(Name = "Description:")]
+        public string Description { get; set; }
+
+        [Display(Name = "Gluten Free:")]
+        public bool GlutenFree { get; set; }
+
+        [Display(Name = "Caloric Value:")]
+        public int CaloricValue { get; set; }
+
+        [Range(1, 15)]
+        [Required(ErrorMessage = "Please enter a cupcake price")]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Price:")]
+        public double? Price { get; set; }
+
+        [Required(ErrorMessage = "Please select a bakery")]
+        public int? BakeryId { get; set; }
+
+        [JsonIgnore]
+        [NotMapped]
+        public virtual Bakery Bakery { get; set; }
+    }
+}
