4835ae7d00c3f64b5a19909518421a28a88d738e
Loredana Radu <loredana.radu@scoalainformala.ro>
2024-10-25 09:45:05 +0300

    test

--- Recap1/Quiz/Program.cs
+++ Recap1/Quiz/Program.cs
@@ -14,6 +14,21 @@
             //a.Method1();
 
             Orange mySweetOrange = new Orange();
+
+            Generic<String> g = new Generic<String>();
+            g.Field = "Hello";
+            Console.WriteLine(g.Field);
+
+            ////////////////////////
+
+            //string userResponse;
+            //foreach (string name in names)
+            //{
+            //    string userResponse = AskUserIfNameIsOk(name);
+            //    // other code
+            //}
+
+
         }
     }
 
@@ -33,4 +48,45 @@
             // do something else
         }
     }
+
+    public interface IPerson { }
+
+   public class Person 
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return "Person: " + Name + " " + Age;
+        }
+    }
+
+    public class Generic<T>
+    {
+        public T Field;
+    }
+
+
+    public static class GlobalData
+    {
+        public const string ConnStr = "kdajd"; //compile time
+        public static readonly string ConnStr2 = GetConnectionString(); // runtime
+        private static string GetConnectionString() { /* return something here */ return null; }
+    }
+
+    public struct MyTipe // tratat ca un tip primitiv de data
+    {
+        //
+    }
+
+    public sealed class MyClass
+    {
+
+    }
+
+    //public class MyClass2 : MyClass
+    //{
+
+    //}
 }
